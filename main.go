12f1fe27ac0022760a4fe88d6a6007117a4950c0
jihun kim <ahoss20@hotmail.com>
2023-01-19 12:59:28 +0900

    test용

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"chromedp_scrapper/scrapper"
+	"fmt"
 	"github.com/labstack/echo"
 )
 
@@ -10,6 +11,7 @@ func handleHome(ctx echo.Context) error {
 }
 
 func handleCoffeeScrape(ctx echo.Context) error {
+	fmt.Println("야호")
 	scrapper.StartCoffeeScrape()
 	return ctx.Attachment("coffee.json", "coffee.json")
 }
